d5d6caf8cc7446b59c592d01bfb6025d4223dbe2
Gaurav <gauravsarage.1902@gmail.com>
2023-07-12 03:26:35 +0530

    added App css, used gpt to help :)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from "react-router-dom";
 import { Box } from '@mui/material';
 
+import "./App.css";
 
 import ExerciseDetail from "./pages/ExerciseDetail";
 import Home from "./pages/Home";
--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -6,26 +6,42 @@ import HeroBannerImage from "../assets/images/banner.png"
 const HeroBanner = () => {
   return (
     <Box sx={{
-      mt: { lg: "210px", xs: "70px" },
+      mt: { lg: "212px", xs: "70px" },
       ml: { sm: "50px" }
-    }} position="relative" p="20px" >
-      <Typography color="#FF2625" fontWeight="650" fontSize="25px">
+    }} position="relative" p="5px" >
+      <Typography color="#FF2625" fontWeight="900" fontSize="30px">
         Fitness Club App
       </Typography>
 
       <Typography fontWeight="700"
-      sx={{ fontSize: { lg: "45px", xs: "40px" }}}
+      sx={{ fontSize: { lg: "45px", xs: "40px" }
+      }}
+      mb="22px" mt="30px"      
       >
         Sweat, Smile <br /> and Repeat
       </Typography>
 
-      <Typography fontSize="23px" lineHeight="34px" mb={2}>
+      <Typography fontSize="23px" lineHeight="34px" mb={4}>
         Check out our most effective exercises! 
       </Typography>
 
-      <Button variant="contained" color="error" href="#exercises">
+      <Button variant="contained" color="error" href="#exercises"
+      sx={{ backgroundColor: "FF2625" }}
+      >
         Explore Exercises
       </Button>
+
+      <Typography
+        fontWeight={600}
+        color="#FF2625"
+        sx={{
+          opacity: 0.1,
+          display: { lg: "block", xs: "none" }
+        }}
+        fontSize="200px"
+      >
+        Exercise
+      </Typography>
 
       <img src={HeroBannerImage} alt="Banner Image" className="hero-banner-img" />
     </Box>
