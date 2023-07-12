88a7d71a4c86ff005ea3e32a54e27897e3e57c24
Gaurav <gauravsarage.1902@gmail.com>
2023-07-12 20:13:47 +0530

    added search exercises component, created file ot fetch data from api and tested it on postman

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,10 +1,72 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
+
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+
 
 const SearchExercises = () => {
+
+  const [search, setSearch] = useState("")
+  const [exercises, setExercises] = useState([])
+
+  const handleSearch = async () => {
+    if(search) {
+      const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+
+      const searchedExercises = exercisesData.filter(
+        (exercise) => exercise.name.toLowerCase().includes(search) || exercise.target.toLowerCase().includes(search) || exercise.equipment.toLowerCase().includes(search) || exercise.bodyPart.toLowerCase().includes(search)
+      );
+
+      setSearch("");
+      setExercises(searchedExercises);
+    }
+  }
+
+
   return (
-    <div>
-      SearchExercises
-    </div>
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography fontWeight={700} sx={{
+        fontSize: { lg: "44px", xs: "30px" }
+      }} mb="50px" textAlign="center">
+        Awesome Exercises You <br /> Should Know
+      </Typography>
+
+      <Box position="relative" mb="72px">
+        <TextField
+        sx={{
+          input: { 
+            fontWeight: "700", 
+            border: "none", 
+            borderRadius: "4px" 
+          },
+          width: { lg: "800px", xs: "350px" },
+          backgroundColor: "#fff", 
+          borderRadius: "40px"
+        }}
+          height="75px"
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
+          placeholder="Search for exercises"
+          type="text"
+        />
+
+        <Button className="search-btn"
+          sx={{
+            bgcolor: "#FF2625",
+            color: "#fff",
+            textTransform: "none",
+            width: { lg: "175px", xs: "81px" },
+            fontSize: { lg: "20px", xs: "14px" },
+            height: "50px",
+            position: "absolute",
+            right: '0'
+          }}
+          onClick={handleSearch}
+        >
+          Search
+        </Button>
+      </Box>
+    </Stack>
   )
 }
 
---
+++ src/utils/fetchData.js
@@ -0,0 +1,16 @@
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+  };
+
+
+export const fetchData = async (url, options) => {
+
+    const response = await fetch(url, options);
+    const data = await response.json();
+
+    return data;
+}
