ea704bc37fd57fe67442b9b35295ee5247ee125a
Gaurav <gauravsarage.1902@gmail.com>
2023-07-13 20:47:49 +0530

    added logic code to scrool left and right across the HorizontalScrollBar

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -7,22 +7,41 @@ import ExerciseCard from "./ExerciseCard";
 import RightArrowIcon from "../assets/icons/right-arrow.png";
 import LeftArrowIcon from "../assets/icons/left-arrow.png";
 
+const LeftArrow = () => {
+  const { scrollPrev } = useContext(VisibilityContext);
 
-const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
   return (
-    <ScrollMenu>
-      { data.map((item) => {
-        <Box
-            key={item.id || item}
-            itemId={item.id || item}
-            title={item.id || item}
-            m="0 40px"
-        >
-            <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
-        </Box>
-      }) }
-    </ScrollMenu>
+    <Typography onClick={() => scrollPrev()} className="right-arrow">
+      <img src={LeftArrowIcon} alt="left-arrow" />
+    </Typography>
+  );
+};
+
+
+const RightArrow = () => {
+  const { scrollNext } = useContext(VisibilityContext);
+
+  return (
+    <Typography onClick={() => scrollNext()} className="left-arrow">
+      <img src={RightArrowIcon} alt="right-arrow" />
+    </Typography>
   )
 }
+
+const HorizontalScrollbar = ({ data, bodyParts, bodyPart, setBodyPart }) => (
+  <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+    {data.map((item) => (
+      <Box
+        key={item.id || item}
+        itemId={item.id || item}
+        title={item.id || item}
+        m="0 40px"
+      >
+        {bodyParts ? <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} /> : <ExerciseCard exercise={item} />}
+      </Box>
+    ))}
+  </ScrollMenu>
+);
+
 
 export default HorizontalScrollbar
