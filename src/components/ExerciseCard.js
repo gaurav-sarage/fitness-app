675c5819747c92914d790d3bf06dd0bc45cc1ddd
Gaurav <gauravsarage.1902@gmail.com>
2023-07-13 21:47:28 +0530

    addec Exercise Card using Link and dynamically route it to it's respective page exercise

---
+++ src/components/ExerciseCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Stack, Typography } from '@mui/material';
+
+const ExerciseCard = ({ exercise }) => {
+  return (
+    <Link className='exercise-card' to={`/exercise/${exercise.id}`}>
+        <img src={exercise.gifUrl} alt={exercise.name} loading='lazy' />
+    </Link>
+  )
+}
+
+export default ExerciseCard
--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Pagination } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 
-const Exercises = () => {
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+import ExerciseCard from './ExerciseCard';
+
+
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
   return (
-    <div>
-      Exercises
-    </div>
+    <Box id="exercises"
+      sx={{mt: { lg: "110px" }}}
+      mt="50px"
+      p="20px"
+    >
+      <Typography variant="h3" mb="46px">
+        Showing Results
+      </Typography>      
+
+      <Stack direction="row" sx={{ gap: { lg: "110px", xs: "50px" }}}
+      flexWrap="wrap"
+      justifyContent="center">
+        {exercises.map((exercise, index) => (
+          <ExerciseCard key={index} exercise={exercise} />
+        ))}
+      </Stack>
+    </Box>
   )
 }
 
