3aa515960342fe95217b39e705adaf547df32cb3
Gaurav <gauravsarage.1902@gmail.com>
2023-07-13 20:53:17 +0530

    random commit

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -7,41 +7,7 @@ import ExerciseCard from "./ExerciseCard";
 import RightArrowIcon from "../assets/icons/right-arrow.png";
 import LeftArrowIcon from "../assets/icons/left-arrow.png";
 
-const LeftArrow = () => {
-  const { scrollPrev } = useContext(VisibilityContext);
 
-  return (
-    <Typography onClick={() => scrollPrev()} className="right-arrow">
-      <img src={LeftArrowIcon} alt="left-arrow" />
-    </Typography>
-  );
-};
-
-
-const RightArrow = () => {
-  const { scrollNext } = useContext(VisibilityContext);
-
-  return (
-    <Typography onClick={() => scrollNext()} className="left-arrow">
-      <img src={RightArrowIcon} alt="right-arrow" />
-    </Typography>
-  )
-}
-
-const HorizontalScrollbar = ({ data, bodyParts, bodyPart, setBodyPart }) => (
-  <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
-    {data.map((item) => (
-      <Box
-        key={item.id || item}
-        itemId={item.id || item}
-        title={item.id || item}
-        m="0 40px"
-      >
-        {bodyParts ? <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} /> : <ExerciseCard exercise={item} />}
-      </Box>
-    ))}
-  </ScrollMenu>
-);
 
 
 export default HorizontalScrollbar
