3d957bb04656850cdbbf5fe0535b5027c970f38e
Gaurav <gauravsarage.1902@gmail.com>
2023-07-14 13:55:30 +0530

    added exercise detail page specifying the icon and name

---
+++ src/components/Detail.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Typography, Stack, Button } from "@mui/material";
+
+import BodyPartImage from "../assets/icons/body-part.png";
+import TargetImage from "../assets/icons/target.png";
+import EquipmentImage from "../assets/icons/equipment.png";
+
+const Detail = ({ exerciseDetail }) => {
+    const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
+
+    const extraDetail = [
+        {
+            icon: BodyPartImage,
+            name: bodyPart,
+        },
+        {
+            icon: TargetImage,
+            name: target,
+        },
+        {
+            icon: EquipmentImage,
+            name: equipment,
+        },
+    ];
+
+    return (
+        <Stack>
+            
+        </Stack>
+    )
+}
